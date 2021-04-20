cc246ee64da2580073fae145f7fc108cf000fb11
kharnichev <k.harnichev@keepsolid.com>
2021-04-20 13:19:28 +0300

    [upd-some-part] add data for our-projects sections.

--- src/views/OurProjects.vue
+++ src/views/OurProjects.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <our-project class="first-block" />
-    <contact-block />
+    <template v-for="(item, i) in ourProjectList">
+      <our-project :data="[item]"
+                   :key="i"
+                   class="first-block"/>
+    </template>
+    <contact-block/>
   </div>
 </template>
 
@@ -9,7 +13,7 @@
 import {Vue, Component} from "vue-property-decorator";
 import ContactBlock from "@/components/sections/contact-block.vue";
 import OurProject from "@/components/sections/our-projects.vue";
-
+import {ourProjects} from "@/assets/data/index-page.json";
 
 
 @Component({
@@ -19,11 +23,16 @@ import OurProject from "@/components/sections/our-projects.vue";
   }
 })
 export default class OurProjects extends Vue {
+  ourProjectList: Array<any> = [];
 
+  created() {
+    this.ourProjectList = ourProjects;
+  }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss"
+       scoped>
 .first-block {
   padding-top: 100px;
 }
