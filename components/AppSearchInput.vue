d9707decc108655bff7568d7ed98ec318eaf51fb
Nik T <nktovs@gmail.com>
2021-07-26 15:19:01 +0300

    small fix

--- components/AppSearchInput.vue
+++ components/AppSearchInput.vue
@@ -44,3 +44,7 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+</style>
---
+++ components/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="flex">
+    <NuxtLink to="/Blog"><Logo class="logo" /></NuxtLink>
+    <AppSearchInput class="search" />
+  </header>
+</template>
+
+<script lang="ts">
+
+</script>
+<style scoped>
+.logo {
+  margin-bottom: 4px;
+}
+
+.search {
+  margin-left: 8px;
+}
+</style>
