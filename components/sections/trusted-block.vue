6f02605b7441f252699e89b24e31a9e8d020e1fd
WoShiGaoFuShuai <efremovmaxim95@mail.ru>
2022-07-25 10:51:58 +0300

    redone trusted block

--- components/sections/trusted-block.vue
+++ components/sections/trusted-block.vue
@@ -3,7 +3,7 @@
     <dark-bg />
     <stripe-wrapper :color-strip="'dark'" />
     <div class="sb__container container">
-      <div class="sb__left">
+      <div class="title-container">
         <title-block :title="'Trusted By'" :color-icon="'light'" />
         <h4 class="custom-description">
           <span>XDEVS</span> is a <span>web/mobile development</span> team built
@@ -11,9 +11,52 @@
           other and our clients value us.
         </h4>
       </div>
-      <div class="sb__right">
-        <div v-for="(item, i) in cardList" :key="i" class="sb__wrapper-img">
-          <img :src="item.img" :alt="item.alt" width="270" class="sb__card" />
+
+      <div class="sb__customers__card__container">
+        <div
+          v-for="(item, i) in cardList"
+          :key="i"
+          class="sb__customers__card-wrapper"
+        >
+          <div class="sb__customers__card">
+            <div
+              :style="{
+                backgroundColor: item.bgColor,
+              }"
+              class="sb__customers__card-front"
+            >
+              <img
+                :src="item.img"
+                :alt="item.alt"
+                width="270"
+                class="sb__card"
+              />
+            </div>
+            <div
+              class="sb__customers__card-back"
+              :style="{
+                backgroundColor: item.bgColor,
+              }"
+            >
+              <a :href="item.link" target="_blank">
+                <img
+                  :src="item.img"
+                  :alt="item.alt"
+                  width="270"
+                  class="sb__card"
+                />
+              </a>
+
+              <a :href="item.link" target="_blank">
+                <div class="sb__customers__card-back-title">
+                  {{ item.title }}
+                </div>
+                <div class="sb__customers__card-back-description">
+                  {{ item.description }}
+                </div>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -26,53 +69,66 @@ import DarkBg from "~/components/elements/dark-bg.vue";
 import StripeWrapper from "~/components/elements/strip-bg.vue";
 
 //imgs
-import imgs from '~/assets/slider';
-import { defineComponent } from 'vue';
+import imgs from "~/assets/slider";
+import { defineComponent } from "vue";
 
 export default defineComponent({
   components: {
     TitleBlock,
     DarkBg,
-    StripeWrapper
+    StripeWrapper,
   },
   setup() {
     const cardList = [
       {
         img: imgs.tab1,
-        link: "https://www.google.com",
-        alt: "INOXOFT"
+        link: "https://inoxoft.com/",
+        title: "International",
+        description: "software development company",
+        alt: "INOXOFT",
+        bgColor: "#fff",
       },
       {
         img: imgs.tab2,
-        link: "https://www.google.com",
-        alt: "Octopus"
+        link: "http://octopusit.nl/",
+        title: "Software development agency",
+        alt: "Octopus",
+        bgColor: "#fff",
       },
       {
         img: imgs.tab3,
-        link: "https://www.google.com",
-        alt: "Singlead"
+        link: "https://singlead.net/",
+        title: "Digital agency",
+        alt: "Singlead",
+        bgColor: "#D42A06",
       },
       {
         img: imgs.tab4,
-        link: "https://www.google.com",
-        alt: "zitemedia"
+        link: "http://www.zitemedia.com/",
+        title: "Aviation apps",
+        alt: "zitemedia",
+        bgColor: "#fff",
       },
       {
         img: imgs.tab5,
-        link: "https://www.google.com",
-        alt: "chargeback"
+        link: "http://chargebackoptimizer.com/",
+        title: "Fintech startup",
+        alt: "chargeback",
+        bgColor: "#fff",
       },
       {
         img: imgs.tab6,
-        link: "https://www.google.com",
-        alt: "DevLogics"
-      }
+        link: "https://devlogics.nl/",
+        title: "Innovative apps",
+        alt: "DevLogics",
+        bgColor: "#44067F",
+      },
     ];
     return {
-      cardList
-    }
-  }
-})
+      cardList,
+    };
+  },
+});
 </script>
 
 <style lang="scss" scoped>
@@ -82,8 +138,9 @@ export default defineComponent({
   &__container.container {
     padding: 45px 1rem 160px 1rem;
     display: flex;
-    justify-content: space-between;
-    align-items: flex-end;
+    flex-direction: column;
+    // justify-content: space-between;
+    // align-items: flex-end;
 
     @include for-large() {
       align-items: baseline;
@@ -99,24 +156,113 @@ export default defineComponent({
     }
   }
 
-  &__right {
-    display: flex;
-    flex-wrap: wrap;
-    max-width: 650px;
-    justify-content: space-between;
-  }
+  // &__right {
+  //   display: flex;
+  //   flex-wrap: wrap;
+  //   max-width: 650px;
+  //   justify-content: space-between;
+  // }
 
-  &__left {
+  // &__left {
+  //   display: flex;
+  //   flex-direction: column;
+  //   padding-right: 1rem;
+
+  //   @include for-middle() {
+  //     width: 100%;
+  //   }
+
+  //   @include for-average() {
+  //     padding: 0 0 1rem 0;
+  //   }
+  // }
+
+  .title-container {
     display: flex;
-    flex-direction: column;
-    padding-right: 1rem;
+    padding: 105px 0 70px;
 
     @include for-middle() {
-      width: 100%;
+      padding: 35px 0 20px;
     }
 
     @include for-average() {
-      padding: 0 0 1rem 0;
+      flex-direction: column;
+    }
+
+    .title {
+      display: inline-block;
+      padding: 0;
+
+      @include for-average() {
+        padding-bottom: 20px;
+      }
+    }
+
+    .custom-description {
+      display: inline-block;
+      margin-left: 132px;
+
+      @include for-average() {
+        margin-left: 0;
+      }
+    }
+  }
+
+  &__customers__card__container {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+
+  &__customers__card-wrapper {
+    margin: 20px;
+  }
+
+  &__customers__card {
+    width: 200px;
+    height: 200px;
+    position: relative;
+    perspective: 1000px;
+  }
+
+  &__customers__card-front,
+  &__customers__card-back {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: all 0.5s ease-in-out;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+    background-color: #fff;
+    box-shadow: 0 6px 50px 0 rgba(5, 49, 91, 0.15);
+    border: 1px solid #e7e8e9;
+    border-radius: 10px;
+  }
+
+  &__customers__card-front {
+    img {
+      max-width: 150px;
+      width: 100%;
+    }
+  }
+
+  &__customers__card-back {
+    flex-direction: column;
+    color: #35c5d4;
+    transform: rotateY(180deg);
+    text-align: center;
+    a {
+      color: #35c5d4;
+      cursor: pointer;
+      img {
+        max-width: 150px;
+        width: 100%;
+      }
     }
   }
 
@@ -133,8 +279,15 @@ export default defineComponent({
     }
   }
 
+  &__customers__card-wrapper:hover &__customers__card-front {
+    transform: rotateY(180deg);
+  }
+
+  &__customers__card-wrapper:hover &__customers__card-back {
+    transform: rotateY(1turn);
+  }
+
   &__card {
-    margin: 0 15px;
     max-width: 200px;
 
     @include for-average() {
