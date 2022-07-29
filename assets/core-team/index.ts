df105200a4e13baee06a4d1374b4fab8883863a7
WoShiGaoFuShuai <efremovmaxim95@mail.ru>
2022-07-29 06:48:05 +0300

    XW-3 fixing small mistakes #2

--- assets/core-team/index.ts
+++ assets/core-team/index.ts
@@ -1,6 +1,7 @@
 import xanderImg from './xander.webp'
 import danilaImg from './danila.webp'
 import daniilImg from './daniil.webp'
+import dimaImg from './dima.webp'
 import dimanImg from './diman.webp'
 import nikitaImg from './nikita.webp'
 import ivanImg from './ivan.webp'
@@ -12,6 +13,7 @@ export default {
   xanderImg,
   danilaImg,
   daniilImg,
+  dimaImg,
   nikitaImg,
   dimanImg,
   ivanImg,
