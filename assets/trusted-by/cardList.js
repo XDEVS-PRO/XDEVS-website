7add77d7b639a690b36704404e1aa97954543929
WoShiGaoFuShuai <efremovmaxim95@mail.ru>
2022-12-27 12:18:16 +0300

    XW-3 restyled block

---
+++ assets/trusted-by/cardList.js
@@ -0,0 +1,41 @@
+import imgs from '~/assets/slider';
+
+export const cardList = [
+  {
+    img: imgs.tab1,
+    link: 'https://inoxoft.com/',
+    alt: 'INOXOFT',
+    title: 'International',
+    subtitle: 'software development company',
+  },
+  {
+    img: imgs.tab2,
+    link: 'https://www.octopusit.nl/en',
+    alt: 'Octopus',
+    title: 'Software development agency',
+  },
+  {
+    img: imgs.tab3,
+    link: 'https://singlead.net/',
+    alt: 'Singlead',
+    title: 'Digital agency',
+  },
+  {
+    img: imgs.tab4,
+    link: 'http://www.zitemedia.com/',
+    alt: 'zitemedia',
+    title: 'Aviation apps',
+  },
+  {
+    img: imgs.tab5,
+    link: 'https://the.haus/',
+    alt: 'DevLogics',
+    title: 'Innovative apps',
+  },
+  {
+    img: imgs.tab6,
+    link: 'http://chargebackoptimizer.com/',
+    alt: 'chargeback',
+    title: 'Fintech startup',
+  },
+];
